ecf2484cb8feb661ba8884288233c4d5f0f2d65e
Alex <alex@vitalityvfx.com>
2019-07-25 22:53:20 -0700

    fixup! !REMOVE test_build script

--- test_build.sh
+++ test_build.sh
@@ -2,4 +2,6 @@
 
 DEPS_DIR="/mnt/Profiles/ahughes@netflix.local/_dev/oiio/deps"
 
-make OPENEXR_ROOT_DIR=/vfx ILMBASE_ROOT_DIR=/vfx PYTHON_VERSION="2.7" REDSDK_INCLUDE_DIR="${DEPS_DIR}/Include" REDSDK_LIBRARIES="${DEPS_DIR}/Lib" REDSDK_ROOT="/mnt/Profiles/ahughes@netflix.local/_dev/oiio/deps"
+# BUILDSTATIC=1 
+# LINKSTATIC=1
+make LINK_STATIC=1 OPENEXR_ROOT_DIR=/vfx ILMBASE_ROOT_DIR=/vfx PYTHON_VERSION="2.7" REDSDK_ROOT="/mnt/Profiles/ahughes@netflix.local/_dev/oiio/deps/REDSDK" BOOST_HOME=/tmp/boost_1_53_0/install
